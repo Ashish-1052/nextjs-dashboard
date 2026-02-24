9b6ef27d0b85d2900222a73c95bc1a2b6dde68c8
Ashish-1052 <akedari1052@gmail.com>
2026-02-24 12:07:27 +0530

    chapter 15 done, auth and meta data

---
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -0,0 +1,38 @@
+import Form from '@/app/ui/invoices/edit-form';
+import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
+import { fetchCustomers, fetchInvoiceById } from '@/app/lib/data';
+import { Suspense } from 'react';
+import { notFound } from 'next/navigation';
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: 'Edit Invoice',
+};
+ 
+export default async function Page(props: { params: Promise<{ id: string }> }) {
+  const { id } = await props.params;
+  const [invoice, customers] = await Promise.all([
+    fetchInvoiceById(id),
+    fetchCustomers(),
+  ]);
+  if (!invoice) {
+    notFound()
+  }
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: 'Invoices', href: '/dashboard/invoices' },
+          {
+            label: 'Edit Invoice',
+            href: `/dashboard/invoices/${id}/edit`,
+            active: true,
+          },
+        ]}
+      />
+      <Suspense fallback={<p>Form Loading ...</p>}>
+        <Form invoice={invoice} customers={customers} />
+      </Suspense>
+    </main>
+  );
+}
